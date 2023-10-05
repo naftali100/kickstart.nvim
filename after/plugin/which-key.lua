168d98a0dadc34b2fbb85bfecb0717af393819a8
naftali <48069590+naftali100@users.noreply.github.com>
2023-10-06 01:33:37 +0300

    more docs

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -7,4 +7,6 @@ require('which-key').register({
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['<leader>t'] = { name = '[T]elescope', _ = 'which_key_ignore' },
+  ['<leader>l'] = { name = '[L]SP', _ = 'which_key_ignore' },
 })
